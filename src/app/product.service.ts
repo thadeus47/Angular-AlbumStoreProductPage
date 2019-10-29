30eb2403d4748a886f5c8c8b5953a739c81b6051
thadeus47 <52805067+thadeus47@users.noreply.github.com>
2019-10-30 00:03:52 +0300

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -22,8 +22,10 @@ export class ProductService {
      return this._http.get(this._albumUrl).map((response)=>
      <Album>response.json());
    }
+
    getProducts(): Observable<Product[]> {
-     return this._http.get(this._productsUrl).map(response =>
+     return this._http.get(this._productsUrl).map(response=>
      <Product[]>response.json());
    }
+
 }
