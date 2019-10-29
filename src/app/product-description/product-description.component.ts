85da5d45d28ee238266ed4c322ee129e9edf950e
thadeus47 <52805067+thadeus47@users.noreply.github.com>
2019-10-29 10:11:30 +0300

    Completed module

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { ProductService } from '../product.service';
 
+import { Album } from '../album';
+
 @Component({
   selector: 'app-product-description',
   templateUrl: './product-description.component.html',
@@ -10,7 +12,7 @@ import { ProductService } from '../product.service';
 export class ProductDescriptionComponent implements OnInit {
 
 
-   albumInfo;
+   albumInfo:Album;
 
   constructor(private _productService: ProductService) { }
 
